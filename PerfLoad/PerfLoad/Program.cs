2516d6f6db0257684c715837632ca3d50a9a5745
BriaskCoder <124456496+BriaskCoder@users.noreply.github.com>
2024-07-14 21:57:50 +0100

    Threaeded example

--- PerfLoad/PerfLoad/Program.cs
+++ PerfLoad/PerfLoad/Program.cs
@@ -1,101 +1,167 @@
-﻿using Azure.Messaging.ServiceBus;
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace PerfLoad
+namespace WorkerService
 {
-    internal class Program
+    public class Program
     {
-        static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-            await test();
-
+            CreateHostBuilder(args).Build().Run();
         }
 
-        private static async Task test()
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+
+                    // Application Insights
+
+                    // Add custom TelemetryInitializer
+                    //services.AddSingleton<ITelemetryInitializer, MyCustomTelemetryInitializer>();
+
+                    //// Add custom TelemetryProcessor
+                    //services.AddApplicationInsightsTelemetryProcessor<MyCustomTelemetryProcessor>();
+
+                    //// Example on Configuring TelemetryModules.
+                    //// [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Not a real api key, this is example code.")]
+                    //services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((mod, opt) => mod.AuthenticationApiKey = "InstrumentationKey=f6307a4b-4e1a-4448-864e-e9d96cafb529;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=f7f334f3-77ba-4f0e-930a-ebc883a9afaf");
+
+                    // instrumentation key is read automatically from appsettings.json
+                    services.AddApplicationInsightsTelemetryWorkerService();
+                });
+
+        internal class MyCustomTelemetryInitializer : ITelemetryInitializer
         {
-
-            // the client that owns the connection and can be used to create senders and receivers
-//            ServiceBusClient client;
-
-            // the sender used to publish messages to the queue
-            ServiceBusSender sender;
-
-            // number of messages to be sent to the queue
-            const int numOfMessages = 10;
-
-            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
-            // of the application, which is best practice when messages are being published or read
-            // regularly.
-            //
-            // set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
-            // If you use the default AmqpTcp, you will need to make sure that the ports 5671 and 5672 are open
-
-            // TODO: Replace the <NAMESPACE-CONNECTION-STRING> and <QUEUE-NAME> placeholders
-
-            try
+            public void Initialize(ITelemetry telemetry)
             {
-                var connectionString = "Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=";
-
-                ServiceBusClient client = new ServiceBusClient(connectionString);
-
-                var queueOrTopicName = "brwstestqueue1";
-
-                var queueSender = client.CreateSender(queueOrTopicName);
-                Console.WriteLine($"Sending messages to the queue: {queueOrTopicName}");
-
-                for (int i = 0; i < numOfMessages; i++)
-                {
-                    await queueSender.SendMessageAsync(new ServiceBusMessage($"This is a single message that we sent {i}"));//.GetAwaiter().GetResult();
-                }
-                
-                Console.WriteLine($"Sent {numOfMessages} messages to the queue: {queueOrTopicName}");
-                Console.WriteLine("Press any key to end the application");
-                Console.ReadKey();
-
-            //    client = new ServiceBusClient("Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=", clientOptions);
-                sender = client.CreateSender("brwstestqueue1");
-
-                
-
-                //Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=
-
-                // create a batch 
-                using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
-
-                for (int i = 1; i <= numOfMessages; i++)
-                {
-                    // try adding a message to the batch
-                    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
-                    {
-                        // if it is too large for the batch
-                        throw new Exception($"The message {i} is too large to fit in the batch.");
-                    }
-                }
-
-                try
-                {
-                    // Use the producer client to send the batch of messages to the Service Bus queue
-                    await sender.SendMessagesAsync(messageBatch);
-                    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
-                }
-                finally
-                {
-                    // Calling DisposeAsync on client types is required to ensure that network
-                    // resources and other unmanaged objects are properly cleaned up.
-                    await sender.DisposeAsync();
-                    await client.DisposeAsync();
-                }
-
-
+                // Replace with actual properties.
+                (telemetry as ISupportProperties).Properties["MyCustomKey"] = "MyCustomValue";
             }
-            catch (Exception e)
+        }
+
+        internal class MyCustomTelemetryProcessor : ITelemetryProcessor
+        {
+            ITelemetryProcessor next;
+
+            public MyCustomTelemetryProcessor(ITelemetryProcessor next)
             {
-                int i = 0;
+                this.next = next;
             }
 
-            Console.WriteLine("Press any key to end the application");
-            Console.ReadKey();
+            public void Process(ITelemetry item)
+            {
+                // Example processor - not filtering out anything.
+                // This should be replaced with actual logic.
+                this.next.Process(item);
+            }
         }
     }
 }
+
+//using Azure.Messaging.ServiceBus;
+
+//namespace PerfLoad
+//{
+//    internal class Program
+//    {
+//        static async Task Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+
+//            await test();
+
+//        }
+
+//        private static async Task test()
+//        {
+
+//            // the client that owns the connection and can be used to create senders and receivers
+////            ServiceBusClient client;
+
+//            // the sender used to publish messages to the queue
+//            ServiceBusSender sender;
+
+//            // number of messages to be sent to the queue
+//            const int numOfMessages = 10;
+
+//            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+//            // of the application, which is best practice when messages are being published or read
+//            // regularly.
+//            //
+//            // set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
+//            // If you use the default AmqpTcp, you will need to make sure that the ports 5671 and 5672 are open
+
+//            // TODO: Replace the <NAMESPACE-CONNECTION-STRING> and <QUEUE-NAME> placeholders
+
+//            try
+//            {
+//                var connectionString = "Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=";
+
+//                ServiceBusClient client = new ServiceBusClient(connectionString);
+
+//                var queueOrTopicName = "brwstestqueue1";
+
+//                var queueSender = client.CreateSender(queueOrTopicName);
+//                Console.WriteLine($"Sending messages to the queue: {queueOrTopicName}");
+
+//                for (int i = 0; i < numOfMessages; i++)
+//                {
+//                    await queueSender.SendMessageAsync(new ServiceBusMessage($"This is a single message that we sent {i}"));//.GetAwaiter().GetResult();
+//                }
+                
+//                Console.WriteLine($"Sent {numOfMessages} messages to the queue: {queueOrTopicName}");
+//                Console.WriteLine("Press any key to end the application");
+//                Console.ReadKey();
+
+//            //    client = new ServiceBusClient("Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=", clientOptions);
+//                sender = client.CreateSender("brwstestqueue1");
+
+                
+
+//                //Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=
+
+//                // create a batch 
+//                using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+//                for (int i = 1; i <= numOfMessages; i++)
+//                {
+//                    // try adding a message to the batch
+//                    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+//                    {
+//                        // if it is too large for the batch
+//                        throw new Exception($"The message {i} is too large to fit in the batch.");
+//                    }
+//                }
+
+//                try
+//                {
+//                    // Use the producer client to send the batch of messages to the Service Bus queue
+//                    await sender.SendMessagesAsync(messageBatch);
+//                    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+//                }
+//                finally
+//                {
+//                    // Calling DisposeAsync on client types is required to ensure that network
+//                    // resources and other unmanaged objects are properly cleaned up.
+//                    await sender.DisposeAsync();
+//                    await client.DisposeAsync();
+//                }
+
+
+//            }
+//            catch (Exception e)
+//            {
+//                int i = 0;
+//            }
+
+//            Console.WriteLine("Press any key to end the application");
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ PerfLoad/PerfLoad/Worker.cs
@@ -0,0 +1,140 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private TelemetryClient tc;
+        private static HttpClient httpClient = new HttpClient();
+
+        public Worker(ILogger<Worker> logger, TelemetryClient tc)
+        {
+            _logger = logger;
+            this.tc = tc;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
+            Thread thread1 = new Thread(SendMessages);
+            Thread thread2 = new Thread(SendMessages);
+            Thread thread3 = new Thread(SendMessages);
+            Thread thread4 = new Thread(SendMessages);
+
+            thread1.Start(1);
+            thread2.Start(2);
+            thread3.Start(3);
+            thread4.Start(4);
+
+            Thread thread11 = new Thread(SendMessages);
+            Thread thread21 = new Thread(SendMessages);
+            Thread thread31 = new Thread(SendMessages);
+            Thread thread41 = new Thread(SendMessages);
+
+            thread11.Start(11);
+            thread21.Start(21);
+            thread31.Start(31);
+            thread41.Start(41);
+
+            // By default only Warning of above is captured.
+            // However the following Info level will be captured by ApplicationInsights,
+            // as appsettings.json configured Information level for the category 'WorkerServiceSampleWithApplicationInsights.Worker'
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                //using (tc.StartOperation<RequestTelemetry>("workeroperation"))
+                //{
+                //    var res = httpClient.GetAsync("https://bing.com").Result.StatusCode;
+                //    _logger.LogInformation("bing http call completed with status:" + res);
+                //}
+
+                // number of messages to be sent to the queue
+                const int numOfMessages = 1000;
+
+                // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+                // of the application, which is best practice when messages are being published or read
+                // regularly.
+                //
+                // set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
+                // If you use the default AmqpTcp, you will need to make sure that the ports 5671 and 5672 are open
+
+                // TODO: Replace the <NAMESPACE-CONNECTION-STRING> and <QUEUE-NAME> placeholders
+
+                //try
+                //{
+                //    var connectionString = "Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=";
+
+                //    await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+                //    {
+                //        var queueOrTopicName = "brwstestqueue1";
+
+                //        var queueSender = client.CreateSender(queueOrTopicName);
+                //        Console.WriteLine($"Sending messages to the queue: {queueOrTopicName}");
+
+                //        for (int i = 0; i < numOfMessages; i++)
+                //        {
+                //            await queueSender.SendMessageAsync(new ServiceBusMessage($"This is a single message that we sent {i}"));                            
+                //        }
+                //    }
+
+                //    //Console.WriteLine($"Sent {numOfMessages} messages to the queue: {queueOrTopicName}");
+                //    //Console.WriteLine("Press any key to end the application");
+                //    //Console.ReadKey();
+
+                //    await Task.Delay(1000, stoppingToken);
+                //}
+                //catch (Exception ex)
+                //{
+                //    tc.TrackException(ex);
+                //    tc.Flush();
+                //    throw;
+                //}
+            //}
+        }
+
+        static void SendMessages(Object stateInfo)
+        {
+            int index = (int)stateInfo;
+
+            int numOfMessages = 1000;
+            var connectionString = "Endpoint=sb://brwstestnamespace1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hCtK3tapXto2J3S2ix5FGsyxR0/UmbZ5q+ASbPFRfVk=";
+
+            ServiceBusClient client = new ServiceBusClient(connectionString);
+
+            var queueOrTopicName = "brwstestqueue1";
+
+            var queueSender = client.CreateSender(queueOrTopicName);
+            Console.WriteLine($"Sending messages to the queue: {queueOrTopicName} : Thread {index}");
+
+            try
+            {
+                for (int i = 0; i < numOfMessages; i++)
+                {
+                    queueSender.SendMessageAsync(new ServiceBusMessage($"This is a single message that we sent {i}")).Wait();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                queueSender.DisposeAsync();
+                client.DisposeAsync();
+            }
+
+            Console.WriteLine($"FINISHED!! Sending messages to the queue: {queueOrTopicName} : Thread {index}");
+        }
+    }
+}
