efac336dad21b2c37c63eb70877448ac4b4457ca
BriaskCoder <124456496+BriaskCoder@users.noreply.github.com>
2024-09-15 21:09:10 +0100

    Corrected PerfLoad Docker config

---
+++ PerfLoad/PerfThreadInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerService
+{
+    internal class PerfThreadInfo
+    {
+        public int Id { get; set; }
+        public int NumberMessages { get; set; }
+        public int Size { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+        public long Elapsed { get; set; }
+        public ILogger logger { get; set; }
+
+    }
+}
