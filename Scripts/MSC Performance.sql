ded966c3decf5d4e921d511a0bb50e0c615bd5f9
BriaskCoder <124456496+BriaskCoder@users.noreply.github.com>
2024-10-07 19:42:21 +0100

    add sql scripts

--- Scripts/MSC Performance.sql
+++ Scripts/MSC Performance.sql
@@ -1,4 +1,4 @@
-SELECT top 1 *
+SELECT top 10 *
   FROM [dbo].[PerfThreadInfo]
   order by id desc
 
@@ -12,7 +12,12 @@ select top 1 *
 from Runs
 order by id desc
 
+SELECT *
+FROM [dbo].[PerfThreadInfo]
+where RunId = 101
+order by id desc
+
 select p.RunId,  sum(numcreated) as Created, sum(p.ActualRate) as Rate
 FROM [dbo].[PerfThreadInfo] p
-where RunId = 97 or RunId = 101
+where RunId = 97 or RunId = 105 or RunId = 106
 group by p.RunId
